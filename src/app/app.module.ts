a28edee86de0b9500fa59e93110b7b8b6cb28301
sahil13 <sahil834@gmail.com>
2020-05-07 11:36:16 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -12,6 +12,7 @@ import { HomeComponent } from '../home/home.component';
   imports:      [ 
     BrowserModule, 
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([{ path:"",component : HomeComponent }]) 
     ],
   declarations: [ 
