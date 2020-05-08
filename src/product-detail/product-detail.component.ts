3211060c7f9bd5b34977951fb8edd0d787d0d819
sahil13 <sahil834@gmail.com>
2020-05-08 12:07:21 +0530

    4 files modified

--- src/product-detail/product-detail.component.ts
+++ src/product-detail/product-detail.component.ts
@@ -11,20 +11,20 @@ export class ProductDetailComponent implements OnInit {
 product=[{
 	"name": "lux",
 	"desc": "Indulge in a sensorial experience with breakthrough Floral Beauty Oil that gives you a fragrance bloom two times better than real flowers.",
-	"location": [{
+	"location": {
 		"street": "123",
 		"city": "gurgaon"
-	}],
+	},
 	"img": "https://www.hul.co.in/Images/India-LUX-new-hand-wash-collection-310818_tcm1255-525690.jpg",
 	"price": "70"
 },{
-	"name": "lux",
+	"name": "Nirma",
 	"desc": "Indulge in a sensorial experience with breakthrough Floral Beauty Oil that gives you a fragrance bloom two times better than real flowers.",
-	"location": [{
+	"location": {
 		"street": "123",
 		"city": "gurgaon"
-	}],
-	"img": "https://www.hul.co.in/Images/India-LUX-new-hand-wash-collection-310818_tcm1255-525690.jpg",
+	},
+	"img": "https://i.ebayimg.com/images/g/BkgAAOSw-BtcxTro/s-l300.jpg",
 	"price": "70"
 }];
 
