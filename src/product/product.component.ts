6237eb97f1deb14077b33d3ed10369965057e17b
sahil13 <sahil834@gmail.com>
2020-05-08 12:09:57 +0530

    1 file modified

--- src/product/product.component.ts
+++ src/product/product.component.ts
@@ -18,13 +18,13 @@ product=[{
 	"img": "https://www.hul.co.in/Images/India-LUX-new-hand-wash-collection-310818_tcm1255-525690.jpg",
 	"price": "70"
 },{
-	"name": "lux",
+	"name": "Nirma",
 	"desc": "Indulge in a sensorial experience with breakthrough Floral Beauty Oil that gives you a fragrance bloom two times better than real flowers.",
 	"location": [{
 		"street": "123",
 		"city": "gurgaon"
 	}],
-	"img": "https://www.hul.co.in/Images/India-LUX-new-hand-wash-collection-310818_tcm1255-525690.jpg",
+	"img": "https://i.ebayimg.com/images/g/BkgAAOSw-BtcxTro/s-l300.jpg",
 	"price": "70"
 }];
 
