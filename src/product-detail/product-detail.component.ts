2d1b5192e8e5275602e2735eb357b2e765befa66
sahil13 <sahil834@gmail.com>
2020-05-08 11:34:03 +0530

    4 files modified, 1 added

--- src/product-detail/product-detail.component.ts
+++ src/product-detail/product-detail.component.ts
@@ -7,7 +7,28 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-@Input() product;
+product=[{
+  "id":1,
+	"name": "lux",
+	"desc": "Indulge in a sensorial experience with breakthrough Floral Beauty Oil that gives you a fragrance bloom two times better than real flowers.",
+	"location": [{
+		"street": "123",
+		"city": "gurgaon"
+	}],
+	"img": "https://www.hul.co.in/Images/India-LUX-new-hand-wash-collection-310818_tcm1255-525690.jpg",
+	"price": "70"
+},{
+  "id":2,
+	"name": "lux",
+	"desc": "Indulge in a sensorial experience with breakthrough Floral Beauty Oil that gives you a fragrance bloom two times better than real flowers.",
+	"location": [{
+		"street": "123",
+		"city": "gurgaon"
+	}],
+	"img": "https://www.hul.co.in/Images/India-LUX-new-hand-wash-collection-310818_tcm1255-525690.jpg",
+	"price": "70"
+}];
+
   constructor() {
     console.log(this.product);
    }
